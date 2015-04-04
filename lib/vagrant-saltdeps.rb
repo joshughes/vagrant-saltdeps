1a2c28525e990baa7e06933f1eb77d4954f94440
Jeff Storey <jstorey@net-demon.com>
2015-04-03 21:04:40 -0600

    add I18n load path.

--- lib/vagrant-saltdeps.rb
+++ lib/vagrant-saltdeps.rb
@@ -6,6 +6,6 @@ module VagrantPlugins
     def self.source_root
       @source_root ||= Pathname.new(File.expand_path("../../", __FILE__))
     end
-    
-  end
+     I18n.load_path += Dir[File.expand_path("../../locales/*{rb,yml}", __FILE__)]
+   end
 end
